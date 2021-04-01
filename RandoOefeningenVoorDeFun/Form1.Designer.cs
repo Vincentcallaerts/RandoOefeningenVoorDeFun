fc9af1ba555123dbc23e5944ea8debf1168c7fad
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-04-01 12:33:07 +0200

    Add project files.

---
+++ RandoOefeningenVoorDeFun/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace RandoOefeningenVoorDeFun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKnop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKnop
+            // 
+            this.btnKnop.Location = new System.Drawing.Point(323, 200);
+            this.btnKnop.Name = "btnKnop";
+            this.btnKnop.Size = new System.Drawing.Size(120, 20);
+            this.btnKnop.TabIndex = 0;
+            this.btnKnop.Text = "Color Fun";
+            this.btnKnop.UseVisualStyleBackColor = true;
+            this.btnKnop.Click += new System.EventHandler(this.btnKnop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.btnKnop);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKnop;
+    }
+}
+
---
+++ RandoOefeningenVoorDeFun/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandoOefeningenVoorDeFun
+{
+    public partial class Form1 : Form
+    {
+        private Random rng;
+        private Graphics g;
+        public Form1()
+        {
+            
+            
+            InitializeComponent();
+            g = CreateGraphics();
+            
+            rng = new Random();
+        }
+
+        private void btnKnop_Click(object sender, EventArgs e)
+        {         
+            Controls.Remove(btnKnop);
+            g.Clear(Color.Black);
+            while (true)
+            {
+                DrawPoly(10);
+            }          
+        }
+        private Point PointGen() 
+        {
+
+            Point temp = new Point(rng.Next(800), rng.Next(500));
+            return temp;
+        
+        }
+        private Point[] PointsForPoly()
+        {
+            Point[] temp = new Point[20];
+            for (int i = 0; i < 20; i++)
+            {
+                temp[i] = PointGen();
+            }
+            return temp;
+        }
+        private void DrawPoly(int counter)
+        {
+            
+            for (int i = 0; i < counter; i++)
+            {                              
+                Pen myPen = new Pen(Color.FromArgb(rng.Next(256), rng.Next(256), rng.Next(256)));
+                
+                g.DrawPolygon(myPen, PointsForPoly());
+                
+            }                       
+        }
+    }
+}
