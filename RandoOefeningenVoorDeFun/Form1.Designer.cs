55b0c198a6e497e3a1759dda2cb51a41122c5197
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-04-01 19:19:07 +0200

    op

--- RandoOefeningenVoorDeFun/Form1.Designer.cs
+++ RandoOefeningenVoorDeFun/Form1.Designer.cs
@@ -30,12 +30,14 @@ namespace RandoOefeningenVoorDeFun
         private void InitializeComponent()
         {
             this.btnKnop = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnRainbow = new System.Windows.Forms.Button();
+            this.btnActualRainbow = new System.Windows.Forms.Button();
+            this.btnCookie = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnKnop
             // 
-            this.btnKnop.Location = new System.Drawing.Point(323, 200);
+            this.btnKnop.Location = new System.Drawing.Point(12, 12);
             this.btnKnop.Name = "btnKnop";
             this.btnKnop.Size = new System.Drawing.Size(120, 20);
             this.btnKnop.TabIndex = 0;
@@ -43,25 +45,49 @@ namespace RandoOefeningenVoorDeFun
             this.btnKnop.UseVisualStyleBackColor = true;
             this.btnKnop.Click += new System.EventHandler(this.btnKnop_Click);
             // 
-            // button1
+            // btnRainbow
             // 
-            this.button1.Location = new System.Drawing.Point(323, 226);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(120, 20);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Rainbow mode";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnRainbow.Location = new System.Drawing.Point(12, 38);
+            this.btnRainbow.Name = "btnRainbow";
+            this.btnRainbow.Size = new System.Drawing.Size(120, 20);
+            this.btnRainbow.TabIndex = 1;
+            this.btnRainbow.Text = "Rainbow mode";
+            this.btnRainbow.UseVisualStyleBackColor = true;
+            this.btnRainbow.Click += new System.EventHandler(this.btnRainbow_Click);
+            // 
+            // btnActualRainbow
+            // 
+            this.btnActualRainbow.Location = new System.Drawing.Point(12, 64);
+            this.btnActualRainbow.Name = "btnActualRainbow";
+            this.btnActualRainbow.Size = new System.Drawing.Size(120, 20);
+            this.btnActualRainbow.TabIndex = 2;
+            this.btnActualRainbow.Text = "Actual Rainbow";
+            this.btnActualRainbow.UseVisualStyleBackColor = true;
+            this.btnActualRainbow.Click += new System.EventHandler(this.btnActualRainbow_Click);
+            // 
+            // btnCookie
+            // 
+            this.btnCookie.Location = new System.Drawing.Point(12, 90);
+            this.btnCookie.Name = "btnCookie";
+            this.btnCookie.Size = new System.Drawing.Size(120, 20);
+            this.btnCookie.TabIndex = 3;
+            this.btnCookie.Text = "Add Cookie";
+            this.btnCookie.UseVisualStyleBackColor = true;
+            this.btnCookie.Click += new System.EventHandler(this.btnCookie_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCookie);
+            this.Controls.Add(this.btnActualRainbow);
+            this.Controls.Add(this.btnRainbow);
             this.Controls.Add(this.btnKnop);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -69,7 +95,9 @@ namespace RandoOefeningenVoorDeFun
         #endregion
 
         private System.Windows.Forms.Button btnKnop;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRainbow;
+        private System.Windows.Forms.Button btnActualRainbow;
+        private System.Windows.Forms.Button btnCookie;
     }
 }
 
--- RandoOefeningenVoorDeFun/Form1.cs
+++ RandoOefeningenVoorDeFun/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,29 +16,19 @@ namespace RandoOefeningenVoorDeFun
         private Random rng;
         private Graphics g;
         public Form1()
-        {
-            
-            
+        {                       
             InitializeComponent();
-            g = CreateGraphics();           
+            g = CreateGraphics();
             rng = new Random();
         }
-
-        private void btnKnop_Click(object sender, EventArgs e)
-        {         
-            Controls.Remove(btnKnop);
-            g.Clear(Color.Black);
-            while (true)
-            {
-                DrawPoly(10);
-            }          
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
         }
         private Point PointGen() 
         {
-
-            Point temp = new Point(rng.Next(800), rng.Next(500));
-            return temp;
-        
+            Point temp = new Point(rng.Next(140,800), rng.Next(500));
+            return temp;        
         }
         private Point[] PointsForPoly()
         {
@@ -49,15 +40,56 @@ namespace RandoOefeningenVoorDeFun
             return temp;
         }
         private void DrawPoly(int counter)
-        {
-            
+        {           
             for (int i = 0; i < counter; i++)
             {                              
-                Pen myPen = new Pen(Color.FromArgb(rng.Next(256), rng.Next(256), rng.Next(256)));
-                
-                g.DrawPolygon(myPen, PointsForPoly());
-                
+                Pen myPen = new Pen(Color.FromArgb(rng.Next(256), rng.Next(256), rng.Next(256)));                
+                g.DrawPolygon(myPen, PointsForPoly());                
             }                       
+        }
+        private void btnKnop_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            DrawPoly(1000);
+        }
+        private void btnRainbow_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                g.Clear(Color.FromArgb(rng.Next(256), rng.Next(256), rng.Next(256)));
+                Thread.Sleep(50);
+            }
+        }
+        private void btnActualRainbow_Click(object sender, EventArgs e)
+        {
+            
+            Pen myPen = new Pen(Color.Red,10);
+            g.DrawArc(myPen, 140, 150, 500, 300, 0, -180);
+            Thread.Sleep(50);
+            myPen = new Pen(Color.Orange, 10);
+            Thread.Sleep(50);
+            g.DrawArc(myPen, 140, 160, 500, 300, 0, -180);
+            Thread.Sleep(50);
+            myPen = new Pen(Color.Yellow, 10);
+            g.DrawArc(myPen, 140, 170, 500, 300, 0, -180);
+            Thread.Sleep(50);
+            myPen = new Pen(Color.Green, 10);
+            g.DrawArc(myPen, 140, 180, 500, 300, 0, -180);
+            Thread.Sleep(50);
+            myPen = new Pen(Color.LightBlue, 10);
+            g.DrawArc(myPen, 140, 190, 500, 300, 0, -180);
+            Thread.Sleep(50);
+            myPen = new Pen(Color.DarkBlue, 10);
+            g.DrawArc(myPen, 140, 200, 500, 300, 0, -180);
+            Thread.Sleep(50);
+            myPen = new Pen(Color.Magenta, 10);
+            g.DrawArc(myPen, 140, 210, 500, 300, 0, -180);
+            Thread.Sleep(50);
+        }
+
+        private void btnCookie_Click(object sender, EventArgs e)
+        {
+            g.DrawIcon(new Icon("iconCookie.ico"), rng.Next(140, 800), rng.Next(500));
         }
     }
 }
